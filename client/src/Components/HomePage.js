bf7f849f062da1a756ffcaa5cfdf34017fd16269
Ethan-Guenther <ethan.guenther98@gmail.com>
2022-08-18 16:05:45 -0500

    homepage messgage

---
+++ client/src/Components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+        <h1>Hello Friends</h1>
+
+        <h2>Don't forget to use branches when pushing in order to prevent merge conflicts</h2>
+    </div>
+  )
+}
+
+export default HomePage
