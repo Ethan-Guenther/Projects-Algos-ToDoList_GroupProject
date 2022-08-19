55ae2503ea6ca57f34e617c836a19d5984119f73
Ethan-Guenther <ethan.guenther98@gmail.com>
2022-08-19 14:16:20 -0500

    Checking Branch

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = () => {
     <div>
         <h1>Hello Friends</h1>
 
-        <h2>Don't forget to use branches when pushing in order to prevent merge conflicts</h2>
+        {/* <h2>Don't forget to use branches when pushing in order to prevent merge conflicts</h2> */}
     </div>
   )
 }
