b6538c5f7e7196cb17d1ba3511353be118bf4c3f
Ethan-Guenther <ethan.guenther98@gmail.com>
2022-08-21 10:13:10 -0500

    added the backend create function to the homepage with axios.

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import {useState} from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const HomePage = () => {
+  const [task, setTask] = useState("");
+  const [errors, setErrors] = useState({}); 
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    axios.post("http://localhost:8000/api/planner", {
+      task,
+    })
+    .then((res) => {
+      console.log(res);
+      setTask(''); 
+    })
+    .catch((err) => {
+      console.log(err);
+      setErrors(err.response.data.errors)
+    })
+  }
+
   return (
     <div className='container'>
       <div>
@@ -11,22 +31,26 @@ const HomePage = () => {
         <div>
           <ul className='NavBar'>
             <li><button><Link to={"/toDoList/daily"}>Show All</Link></button></li>
-            <li><button><Link to={"/toDoList/daily"}>Today's List</Link></button></li>
-            <li><button><Link to={"/toDoList/weekly"}>Weekly List</Link></button></li>
-            <li><button><Link to={"/toDoList/monthly"}>Monthly List</Link></button></li>
+            <li><button><Link to={"/toDoList/daily"}>Today</Link></button></li>
+            <li><button><Link to={"/toDoList/weekly"}>Week</Link></button></li>
+            <li><button><Link to={"/toDoList/monthly"}>Month</Link></button></li>
           </ul>
         </div>
         {/* Use built in date function to create time zone when tasks
         need to be completed. Example: let date = new Date(year, month, day, hours minutes, seconds, milliseconds)*/}
         <div className='create_form_container'>
-            <form>
+            <form onSubmit={onSubmitHandler}>
               <label>Add Plan:</label>
               <br/>
-              <textarea cols={60} rows={15}/>
+              <textarea cols={60} rows={15} name="task" value={task}
+              onChange={(e) => setTask(e.target.value)}/>
+
+              {errors.task ? <p>{errors.task.message}</p>
+              : null }
               <br/>
-              <label>Number of days to complete:</label>
+              <label>Due Date:</label>
               <br/>
-              <input type={"number"}/>
+              <input type={"date"} name="dueDate"/>
               <br/>
               <button>Submit</button>
             </form>
